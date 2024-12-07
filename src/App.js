7ad077cd10c20f80fec506d287ae54c5311d2952
Andy Van Heuit <avanheuit@cytekbio.com>
2024-12-07 13:36:02 -0800

    Add table structure.

--- src/App.js
+++ src/App.js
@@ -2,6 +2,9 @@ import logo from './logo.svg';
 import kip from './mudkipsip.png'
 import React, { useState, useEffect } from 'react';
 import './App.css';
+import { AgGridReact } from 'ag-grid-react'; // React Data Grid Component
+import "ag-grid-community/styles/ag-grid.css"; // Mandatory CSS required by the Data Grid
+import "ag-grid-community/styles/ag-theme-quartz.css"; // Optional Theme applied to the Data Grid
 
 function App() {
   const [currentTime, setCurrentTime] = useState(0);
@@ -16,7 +19,11 @@ function App() {
 
   useEffect(() => {
     fetch('/api/books').then(res => res.json().then(data => {
-      setCurrentBooks(data)
+      var data2 = data.map((entry) => (
+        {'id': entry[0], 'title': entry[1], 'author': entry[2], 'pages': entry[3], 'review': entry[4], 'date': entry[5]}
+      ));
+      console.log(data2);
+      setCurrentBooks(data2);
     }))
   }, []);
   console.log(currentBooks.length)
@@ -29,27 +36,9 @@ function App() {
           {buffoonery? "Stop Clowning" : "Are you Down to Clown?"}
         </button>
         <p className={buffoonery? 'clown-paragraph' : ''}>The current time is {currentTime}.</p>
-        <table className={buffoonery? 'clown-table' : ''}>
-          <thead>
-            <tr>
-              <th>ID</th>
-              <th>Title</th>
-              <th>Author</th>
-              <th>Pages</th>
-              <th>Review</th>
-              <th>Date</th>
-            </tr>
-          </thead>
-          <tbody>
-            {currentBooks.map((book) => (
-              <tr>
-                {book.map((field) => (
-                  <td>{field}</td>
-                ))}
-              </tr>
-            ))}
-          </tbody>
-        </table>
+        <div className={"ag-theme-quartz-dark"} style={{width: '80%', height: '33vh'}}>
+          <AgGridReact rowData={currentBooks} columnDefs={[{field: 'id'}, {field: 'title'}, {field: 'author'}, {field: 'pages'}, {field: 'review'}, {field: 'date'}]} defaultColDef={{flex: 1}}/>
+        </div>
       </header>
     </div>
   );
